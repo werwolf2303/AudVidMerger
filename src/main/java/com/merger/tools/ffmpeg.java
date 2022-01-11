432bc50ac74e016ec33c3c8a31297da2aaca95b7
werwolf2303 <66752411+werwolf2303@users.noreply.github.com>
2022-01-11 20:43:44 +0100

    Update ffmpeg.java

--- src/main/java/com/merger/tools/ffmpeg.java
+++ src/main/java/com/merger/tools/ffmpeg.java
@@ -8,21 +8,21 @@ import java.io.IOException;
 public class ffmpeg {
     public void cutAudio(String videopath,String outputpath) {
         try{
-            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -q:a 0 -map a \"" + outputpath + "cutaudio.mp3\"&explorer " + System.getProperty("user.dir") + "/OUT");
+            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -q:a 0 -map a \"" + outputpath + "cutaudio.mp3\"");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public void cutAudioSaveOnlyVideo(String videopath,String outputpath) {
         try {
-            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -c:v copy -an \"" + outputpath + "noAudio.mp4&explorer " + System.getProperty("user.dir") + "/OUT");
+            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -c:v copy -an \"" + outputpath + "noAudio.mp4\"");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
     public void mergeAudioAndVideo(String videopath,String audiopath, String outputpath) {
         try {
-            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -i \"" + audiopath + "\" \"" + outputpath + "merged.mp4&explorer " + System.getProperty("user.dir") + "/OUT");
+            Runtime.getRuntime().exec("cmd.exe /c start cmd.exe /c " + System.getProperty("user.dir") + new Config().readConfig("ffmpeg") + " -i \"" + videopath + "\" -i \"" + audiopath + "\" \"" + outputpath + "merged.mp4\"");
         } catch (IOException e) {
             e.printStackTrace();
         }
